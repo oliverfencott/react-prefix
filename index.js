61f321f09ad71aa97e3613312982ef580e0f9ffc
Oliver Fencott <oliverfencott@gmail.com>
2015-11-26 23:00:17 +0000

    Reorder function definitions

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 'use strict';
 
 var _ = require('lodash');
-
 var prefixes = ['ms', 'Moz', 'Webkit', 'o'];
-
-function applyPrefixesToProperty(property, propertyValue) {
-  var newProperties = {};
-
-  _.each(prefixes, function(prefixItem) {
-    var newPropertyName = prefixItem + _.capitalize(property);
-    newProperties[newPropertyName] = propertyValue;
-  });
-
-  return newProperties;
-}
-
-function applyPrefixesToEachProperty(styleObject, propertiesToPrefix) {
-  return _.reduce(propertiesToPrefix, function(memo, propertyToPrefix) {
-    return _.merge(memo, applyPrefixesToProperty(propertyToPrefix, styleObject[propertyToPrefix]))
-  }, {});
-}
 
 function prefix(styleObject, propertiesToPrefix) {
   var objectWithPrefixesApplied = _.isArray(propertiesToPrefix)
@@ -28,5 +10,24 @@ function prefix(styleObject, propertiesToPrefix) {
 
   return _.merge(_.clone(styleObject), objectWithPrefixesApplied);
 }
+
+function applyPrefixesToEachProperty(styleObject, propertiesToPrefix) {
+  return _.reduce(propertiesToPrefix, function(memo, propertyToPrefix) {
+    return _.merge(memo, applyPrefixesToProperty(propertyToPrefix, styleObject[propertyToPrefix]))
+  }, {});
+}
+
+function applyPrefixesToProperty(property, propertyValue) {
+  var objectWithPrefixesApplied = {};
+
+  _.each(prefixes, function(prefixItem) {
+    var newPropertyName = prefixItem + _.capitalize(property);
+    objectWithPrefixesApplied[newPropertyName] = propertyValue;
+  });
+
+  return objectWithPrefixesApplied;
+}
+
+
 
 module.exports = prefix;
